5ba4799dcb430980407327419e9130a59c30ccc7
Тимур Курбанов <the_tk_official@outlook.com>
2024-11-15 12:27:32 +0500

    feat: постпроцессинг

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat-css');
+const cssnano = require('cssnano');
 const del = require('del');
 const gulp = require('gulp');
+const htmlMinifier = require('html-minifier');
+const mediaquery = require('postcss-combine-media-query');
 const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
 
 function serve() {
   browserSync.init({
@@ -13,16 +18,37 @@ function serve() {
 }
 
 function html() {
+  const options = {
+    collapseWhitespace: true,
+    keepClosingSlash: true,
+    minifyCSS: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true
+  }
   return gulp.src('src/**/*.html')
     .pipe(plumber())
+    .on('data', function(file) {
+      const bufferFile = Buffer.from(htmlMinifier.minify(file.contents.toString(), options))
+      return file.contents = bufferFile;
+    })
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
+  const plugins = [
+    autoprefixer(),
+    mediaquery(),
+    cssnano()
+  ];
   return gulp.src('src/blocks/**/*.css')
     .pipe(plumber())
     .pipe(concat('bundle.css'))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({stream: true}));
 }
